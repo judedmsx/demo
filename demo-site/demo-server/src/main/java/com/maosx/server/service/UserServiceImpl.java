c6c0d5869fc0ac6eda8b68ec270a8044d2ed782f
maosx <295743106@qq.com>
2017-01-10 15:28:52 +0800

    引入spring 事务机制

--- demo-site/demo-server/src/main/java/com/maosx/server/service/UserServiceImpl.java
+++ demo-site/demo-server/src/main/java/com/maosx/server/service/UserServiceImpl.java
@@ -4,23 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.maosx.api.UserService;
 import com.maosx.entity.User;
 import com.maosx.server.dao.UserDao;
 @Service("userService")
+@Transactional(readOnly=true)
 public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserDao userDao;
 	
+	@Transactional(readOnly=false)
 	public void save(User user) {
 		userDao.save(user);
 	}
 
+	@Transactional(readOnly=false)
 	public void delete(User user) {
 		userDao.delete(user);
 	}
 
+	@Transactional(readOnly=false)
 	public void update(User user) {
 		userDao.update(user);
 	}
